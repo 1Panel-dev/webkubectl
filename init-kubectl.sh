ebd27edd8ac7cb6c50e70a70ea77d28accc50059
liqiang-fit2cloud <liqiang@fit2cloud.com>
2019-10-16 13:56:09 +0800

    NaN

--- init-kubectl.sh
+++ init-kubectl.sh
@@ -2,7 +2,7 @@
 set -e
 
 arg1=$1
-arg2=$1
+arg2=$2
 
 mkdir -p /nonexistent
 mount -t tmpfs tmpfs /nonexistent
@@ -13,6 +13,7 @@ mkdir -p .kube
 if [ -z "${arg2}" ]; then
     echo $(printf "%s" $arg1| base64 -d) > .kube/config
 else
+    export HOME=/nonexistent
     kubectl config set-credentials webkubectl-user --token=${arg2}
     kubectl config set-cluster kubernetes --server=${arg1} --insecure-skip-tls-verify=true
     kubectl config set-context kubernetes --cluster=kubernetes --user=webkubectl-user
