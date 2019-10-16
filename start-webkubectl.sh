84f17886508eed3ccde16353208c51b623ccd563
liqiang-fit2cloud <liqiang@fit2cloud.com>
2019-10-16 19:12:42 +0800

    NaN

--- start-webkubectl.sh
+++ start-webkubectl.sh
@@ -3,8 +3,7 @@
 echo KUBECTL_INSECURE_SKIP_TLS_VERIFY=${KUBECTL_INSECURE_SKIP_TLS_VERIFY}
 
 if [ ${KUBECTL_INSECURE_SKIP_TLS_VERIFY} == "true" ];then
-    mv /usr/bin/kubectl /usr/bin/kubectlo
-    echo "alias kubectl='kubectlo --insecure-skip-tls-verify=true'" >> /root/.bashrc
+    echo "alias kubectl='kubectl --insecure-skip-tls-verify=true'" >> /root/.bashrc
 fi
 
 
