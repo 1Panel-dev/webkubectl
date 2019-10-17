73ba93f3ad3c1458e86532f3862d926d2278e102
liqiang-fit2cloud <liqiang@fit2cloud.com>
2019-10-17 15:46:46 +0800

    Add sleep to show error in terminal.

--- start-session.sh
+++ start-session.sh
@@ -6,11 +6,13 @@ all=$*
 if [ -z "${all}" ]; then
     echo No Args provided
     echo Terminal will exit.
+    sleep 1
     exit 1
 fi
 
 if [[ $all == ERROR:* ]]; then
     echo ${all}
+    sleep 1
     exit 1
 fi
 
