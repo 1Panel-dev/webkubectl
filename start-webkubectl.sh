af791f6654e768aab059f0e5b69445c7f2d2eb44
王振 <wangzhen@fit2cloud.com>
2019-10-17 11:23:15 +0800

    enable kubectl autocompletion

--- start-webkubectl.sh
+++ start-webkubectl.sh
@@ -2,6 +2,9 @@
 
 echo KUBECTL_INSECURE_SKIP_TLS_VERIFY=${KUBECTL_INSECURE_SKIP_TLS_VERIFY}
 
+echo "source /usr/share/bash-completion/bash_completion" >> /root/.bashrc
+echo 'source <(kubectl completion bash)' >> /root/.bashrc
+
 if [ ${KUBECTL_INSECURE_SKIP_TLS_VERIFY} == "true" ];then
     echo "alias kubectl='kubectl --insecure-skip-tls-verify=true'" >> /root/.bashrc
 fi
