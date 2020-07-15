f98496631845458be48971a10957653e74cbe3d5
ruster-cn <peiminmin@yeah.net>
2020-07-15 19:44:55 +0800

    test

--- gotty/server/options.go
+++ gotty/server/options.go
@@ -31,7 +31,7 @@ type Options struct {
 	WSOrigin            string           `hcl:"ws_origin" flagName:"ws-origin" flagDescribe:"A regular expression that matches origin URLs to be accepted by WebSocket. No cross origin requests are acceptable by default" default:""`
 	Term                string           `hcl:"term" flagName:"term" flagDescribe:"Terminal name to use on the browser, one of xterm or hterm." default:"xterm"`
 	UseRedisTokenCache  bool             `hcl:"use_redis_token_cache" flagName:"use_redis_token_cache" flagDescribe:"if true,will use redis cache token;if false,will use memory cache token." default:"false"`
-	RedisAddr           string           `hcl:"redis_addr" flagName:"redis_addr" flagDescribe:"if use_redis_token_cache, must set redis_addr  ." default:"127.0.0.1:6634"`
+	RedisAddr           string           `hcl:"redis_addr" flagName:"redis_addr" flagDescribe:"if use_redis_token_cache, must set redis_addr." default:"127.0.0.1:6634"`
 
 	TitleVariables map[string]interface{}
 }
